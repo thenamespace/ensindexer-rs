662a400642d67f833a407ad3cd0dce25cc50cc07
Vedant Chainani <vedantchainani1084@gmail.com>
2026-06-07 03:01:09 +0530

    feat: order entity relations

--- crates/api/src/filters/order/entities.rs
+++ crates/api/src/filters/order/entities.rs
@@ -43,12 +43,40 @@ pub enum DomainOrderBy {
     Labelhash,
     #[graphql(name = "parent")]
     Parent,
+    #[graphql(name = "parent__id")]
+    ParentId,
+    #[graphql(name = "parent__name")]
+    ParentName,
+    #[graphql(name = "parent__labelName")]
+    ParentLabelName,
+    #[graphql(name = "parent__labelhash")]
+    ParentLabelhash,
+    #[graphql(name = "parent__subdomainCount")]
+    ParentSubdomainCount,
+    #[graphql(name = "parent__ttl")]
+    ParentTtl,
+    #[graphql(name = "parent__isMigrated")]
+    ParentIsMigrated,
+    #[graphql(name = "parent__createdAt")]
+    ParentCreatedAt,
+    #[graphql(name = "parent__expiryDate")]
+    ParentExpiryDate,
+    #[graphql(name = "subdomains")]
+    Subdomains,
     #[graphql(name = "subdomainCount")]
     SubdomainCount,
     #[graphql(name = "resolvedAddress")]
     ResolvedAddress,
+    #[graphql(name = "resolvedAddress__id")]
+    ResolvedAddressId,
     #[graphql(name = "resolver")]
     Resolver,
+    #[graphql(name = "resolver__id")]
+    ResolverId,
+    #[graphql(name = "resolver__address")]
+    ResolverAddress,
+    #[graphql(name = "resolver__contentHash")]
+    ResolverContentHash,
     #[graphql(name = "ttl")]
     Ttl,
     #[graphql(name = "isMigrated")]
@@ -57,12 +85,42 @@ pub enum DomainOrderBy {
     CreatedAt,
     #[graphql(name = "owner")]
     Owner,
+    #[graphql(name = "owner__id")]
+    OwnerId,
     #[graphql(name = "registrant")]
     Registrant,
+    #[graphql(name = "registrant__id")]
+    RegistrantId,
     #[graphql(name = "wrappedOwner")]
     WrappedOwner,
+    #[graphql(name = "wrappedOwner__id")]
+    WrappedOwnerId,
     #[graphql(name = "expiryDate")]
     ExpiryDate,
+    #[graphql(name = "registration")]
+    Registration,
+    #[graphql(name = "registration__id")]
+    RegistrationId,
+    #[graphql(name = "registration__registrationDate")]
+    RegistrationRegistrationDate,
+    #[graphql(name = "registration__expiryDate")]
+    RegistrationExpiryDate,
+    #[graphql(name = "registration__cost")]
+    RegistrationCost,
+    #[graphql(name = "registration__labelName")]
+    RegistrationLabelName,
+    #[graphql(name = "wrappedDomain")]
+    WrappedDomain,
+    #[graphql(name = "wrappedDomain__id")]
+    WrappedDomainId,
+    #[graphql(name = "wrappedDomain__expiryDate")]
+    WrappedDomainExpiryDate,
+    #[graphql(name = "wrappedDomain__fuses")]
+    WrappedDomainFuses,
+    #[graphql(name = "wrappedDomain__name")]
+    WrappedDomainName,
+    #[graphql(name = "events")]
+    Events,
 }
 
 impl From<DomainOrderBy> for DomainOrderField {
@@ -73,16 +131,45 @@ impl From<DomainOrderBy> for DomainOrderField {
             DomainOrderBy::LabelName => Self::LabelName,
             DomainOrderBy::Labelhash => Self::Labelhash,
             DomainOrderBy::Parent => Self::Parent,
+            DomainOrderBy::ParentId => Self::ParentId,
+            DomainOrderBy::ParentName => Self::ParentName,
+            DomainOrderBy::ParentLabelName => Self::ParentLabelName,
+            DomainOrderBy::ParentLabelhash => Self::ParentLabelhash,
+            DomainOrderBy::ParentSubdomainCount => Self::ParentSubdomainCount,
+            DomainOrderBy::ParentTtl => Self::ParentTtl,
+            DomainOrderBy::ParentIsMigrated => Self::ParentIsMigrated,
+            DomainOrderBy::ParentCreatedAt => Self::ParentCreatedAt,
+            DomainOrderBy::ParentExpiryDate => Self::ParentExpiryDate,
+            DomainOrderBy::Subdomains => Self::Subdomains,
             DomainOrderBy::SubdomainCount => Self::SubdomainCount,
             DomainOrderBy::ResolvedAddress => Self::ResolvedAddress,
+            DomainOrderBy::ResolvedAddressId => Self::ResolvedAddressId,
             DomainOrderBy::Resolver => Self::Resolver,
+            DomainOrderBy::ResolverId => Self::ResolverId,
+            DomainOrderBy::ResolverAddress => Self::ResolverAddress,
+            DomainOrderBy::ResolverContentHash => Self::ResolverContentHash,
             DomainOrderBy::Ttl => Self::Ttl,
             DomainOrderBy::IsMigrated => Self::IsMigrated,
             DomainOrderBy::CreatedAt => Self::CreatedAt,
             DomainOrderBy::Owner => Self::Owner,
+            DomainOrderBy::OwnerId => Self::OwnerId,
             DomainOrderBy::Registrant => Self::Registrant,
+            DomainOrderBy::RegistrantId => Self::RegistrantId,
             DomainOrderBy::WrappedOwner => Self::WrappedOwner,
+            DomainOrderBy::WrappedOwnerId => Self::WrappedOwnerId,
             DomainOrderBy::ExpiryDate => Self::ExpiryDate,
+            DomainOrderBy::Registration => Self::Registration,
+            DomainOrderBy::RegistrationId => Self::RegistrationId,
+            DomainOrderBy::RegistrationRegistrationDate => Self::RegistrationRegistrationDate,
+            DomainOrderBy::RegistrationExpiryDate => Self::RegistrationExpiryDate,
+            DomainOrderBy::RegistrationCost => Self::RegistrationCost,
+            DomainOrderBy::RegistrationLabelName => Self::RegistrationLabelName,
+            DomainOrderBy::WrappedDomain => Self::WrappedDomain,
+            DomainOrderBy::WrappedDomainId => Self::WrappedDomainId,
+            DomainOrderBy::WrappedDomainExpiryDate => Self::WrappedDomainExpiryDate,
+            DomainOrderBy::WrappedDomainFuses => Self::WrappedDomainFuses,
+            DomainOrderBy::WrappedDomainName => Self::WrappedDomainName,
+            DomainOrderBy::Events => Self::Events,
         }
     }
 }
@@ -95,6 +182,24 @@ pub enum RegistrationOrderBy {
     Id,
     #[graphql(name = "domain")]
     Domain,
+    #[graphql(name = "domain__id")]
+    DomainId,
+    #[graphql(name = "domain__name")]
+    DomainName,
+    #[graphql(name = "domain__labelName")]
+    DomainLabelName,
+    #[graphql(name = "domain__labelhash")]
+    DomainLabelhash,
+    #[graphql(name = "domain__subdomainCount")]
+    DomainSubdomainCount,
+    #[graphql(name = "domain__ttl")]
+    DomainTtl,
+    #[graphql(name = "domain__isMigrated")]
+    DomainIsMigrated,
+    #[graphql(name = "domain__createdAt")]
+    DomainCreatedAt,
+    #[graphql(name = "domain__expiryDate")]
+    DomainExpiryDate,
     #[graphql(name = "registrationDate")]
     RegistrationDate,
     #[graphql(name = "expiryDate")]
@@ -103,8 +208,12 @@ pub enum RegistrationOrderBy {
     Cost,
     #[graphql(name = "registrant")]
     Registrant,
+    #[graphql(name = "registrant__id")]
+    RegistrantId,
     #[graphql(name = "labelName")]
     LabelName,
+    #[graphql(name = "events")]
+    Events,
 }
 
 impl From<RegistrationOrderBy> for RegistrationOrderField {
@@ -112,11 +221,22 @@ impl From<RegistrationOrderBy> for RegistrationOrderField {
         match value {
             RegistrationOrderBy::Id => Self::Id,
             RegistrationOrderBy::Domain => Self::Domain,
+            RegistrationOrderBy::DomainId => Self::DomainId,
+            RegistrationOrderBy::DomainName => Self::DomainName,
+            RegistrationOrderBy::DomainLabelName => Self::DomainLabelName,
+            RegistrationOrderBy::DomainLabelhash => Self::DomainLabelhash,
+            RegistrationOrderBy::DomainSubdomainCount => Self::DomainSubdomainCount,
+            RegistrationOrderBy::DomainTtl => Self::DomainTtl,
+            RegistrationOrderBy::DomainIsMigrated => Self::DomainIsMigrated,
+            RegistrationOrderBy::DomainCreatedAt => Self::DomainCreatedAt,
+            RegistrationOrderBy::DomainExpiryDate => Self::DomainExpiryDate,
             RegistrationOrderBy::RegistrationDate => Self::RegistrationDate,
             RegistrationOrderBy::ExpiryDate => Self::ExpiryDate,
             RegistrationOrderBy::Cost => Self::Cost,
             RegistrationOrderBy::Registrant => Self::Registrant,
+            RegistrationOrderBy::RegistrantId => Self::RegistrantId,
             RegistrationOrderBy::LabelName => Self::LabelName,
+            RegistrationOrderBy::Events => Self::Events,
         }
     }
 }
@@ -129,12 +249,32 @@ pub enum WrappedDomainOrderBy {
     Id,
     #[graphql(name = "domain")]
     Domain,
+    #[graphql(name = "domain__id")]
+    DomainId,
+    #[graphql(name = "domain__name")]
+    DomainName,
+    #[graphql(name = "domain__labelName")]
+    DomainLabelName,
+    #[graphql(name = "domain__labelhash")]
+    DomainLabelhash,
+    #[graphql(name = "domain__subdomainCount")]
+    DomainSubdomainCount,
+    #[graphql(name = "domain__ttl")]
+    DomainTtl,
+    #[graphql(name = "domain__isMigrated")]
+    DomainIsMigrated,
+    #[graphql(name = "domain__createdAt")]
+    DomainCreatedAt,
+    #[graphql(name = "domain__expiryDate")]
+    DomainExpiryDate,
     #[graphql(name = "expiryDate")]
     ExpiryDate,
     #[graphql(name = "fuses")]
     Fuses,
     #[graphql(name = "owner")]
     Owner,
+    #[graphql(name = "owner__id")]
+    OwnerId,
     #[graphql(name = "name")]
     Name,
 }
@@ -144,9 +284,19 @@ impl From<WrappedDomainOrderBy> for WrappedDomainOrderField {
         match value {
             WrappedDomainOrderBy::Id => Self::Id,
             WrappedDomainOrderBy::Domain => Self::Domain,
+            WrappedDomainOrderBy::DomainId => Self::DomainId,
+            WrappedDomainOrderBy::DomainName => Self::DomainName,
+            WrappedDomainOrderBy::DomainLabelName => Self::DomainLabelName,
+            WrappedDomainOrderBy::DomainLabelhash => Self::DomainLabelhash,
+            WrappedDomainOrderBy::DomainSubdomainCount => Self::DomainSubdomainCount,
+            WrappedDomainOrderBy::DomainTtl => Self::DomainTtl,
+            WrappedDomainOrderBy::DomainIsMigrated => Self::DomainIsMigrated,
+            WrappedDomainOrderBy::DomainCreatedAt => Self::DomainCreatedAt,
+            WrappedDomainOrderBy::DomainExpiryDate => Self::DomainExpiryDate,
             WrappedDomainOrderBy::ExpiryDate => Self::ExpiryDate,
             WrappedDomainOrderBy::Fuses => Self::Fuses,
             WrappedDomainOrderBy::Owner => Self::Owner,
+            WrappedDomainOrderBy::OwnerId => Self::OwnerId,
             WrappedDomainOrderBy::Name => Self::Name,
         }
     }
@@ -160,12 +310,38 @@ pub enum ResolverOrderBy {
     Id,
     #[graphql(name = "domain")]
     Domain,
+    #[graphql(name = "domain__id")]
+    DomainId,
+    #[graphql(name = "domain__name")]
+    DomainName,
+    #[graphql(name = "domain__labelName")]
+    DomainLabelName,
+    #[graphql(name = "domain__labelhash")]
+    DomainLabelhash,
+    #[graphql(name = "domain__subdomainCount")]
+    DomainSubdomainCount,
+    #[graphql(name = "domain__ttl")]
+    DomainTtl,
+    #[graphql(name = "domain__isMigrated")]
+    DomainIsMigrated,
+    #[graphql(name = "domain__createdAt")]
+    DomainCreatedAt,
+    #[graphql(name = "domain__expiryDate")]
+    DomainExpiryDate,
     #[graphql(name = "address")]
     Address,
     #[graphql(name = "addr")]
     Addr,
+    #[graphql(name = "addr__id")]
+    AddrId,
     #[graphql(name = "contentHash")]
     ContentHash,
+    #[graphql(name = "texts")]
+    Texts,
+    #[graphql(name = "coinTypes")]
+    CoinTypes,
+    #[graphql(name = "events")]
+    Events,
 }
 
 impl From<ResolverOrderBy> for ResolverOrderField {
@@ -173,9 +349,22 @@ impl From<ResolverOrderBy> for ResolverOrderField {
         match value {
             ResolverOrderBy::Id => Self::Id,
             ResolverOrderBy::Domain => Self::Domain,
+            ResolverOrderBy::DomainId => Self::DomainId,
+            ResolverOrderBy::DomainName => Self::DomainName,
+            ResolverOrderBy::DomainLabelName => Self::DomainLabelName,
+            ResolverOrderBy::DomainLabelhash => Self::DomainLabelhash,
+            ResolverOrderBy::DomainSubdomainCount => Self::DomainSubdomainCount,
+            ResolverOrderBy::DomainTtl => Self::DomainTtl,
+            ResolverOrderBy::DomainIsMigrated => Self::DomainIsMigrated,
+            ResolverOrderBy::DomainCreatedAt => Self::DomainCreatedAt,
+            ResolverOrderBy::DomainExpiryDate => Self::DomainExpiryDate,
             ResolverOrderBy::Address => Self::Address,
             ResolverOrderBy::Addr => Self::Addr,
+            ResolverOrderBy::AddrId => Self::AddrId,
             ResolverOrderBy::ContentHash => Self::ContentHash,
+            ResolverOrderBy::Texts => Self::Texts,
+            ResolverOrderBy::CoinTypes => Self::CoinTypes,
+            ResolverOrderBy::Events => Self::Events,
         }
     }
 }
--- crates/storage/src/query/select.rs
+++ crates/storage/src/query/select.rs
@@ -32,16 +32,89 @@ pub(crate) fn domain_order_column(order_by: DomainOrderField) -> &'static str {
         DomainOrderField::LabelName => "label_name",
         DomainOrderField::Labelhash => "labelhash",
         DomainOrderField::Parent => "parent_id",
+        DomainOrderField::ParentId => "(select p.id from domains p where p.id = domains.parent_id)",
+        DomainOrderField::ParentName => {
+            "(select p.name from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentLabelName => {
+            "(select p.label_name from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentLabelhash => {
+            "(select p.labelhash from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentSubdomainCount => {
+            "(select p.subdomain_count from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentTtl => {
+            "(select p.ttl from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentIsMigrated => {
+            "(select p.is_migrated from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentCreatedAt => {
+            "(select p.created_at from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::ParentExpiryDate => {
+            "(select p.expiry_date from domains p where p.id = domains.parent_id)"
+        }
+        DomainOrderField::Subdomains => {
+            "(select count(*) from domains s where s.parent_id = domains.id)"
+        }
         DomainOrderField::SubdomainCount => "subdomain_count",
         DomainOrderField::ResolvedAddress => "resolved_address_id",
+        DomainOrderField::ResolvedAddressId => "resolved_address_id",
         DomainOrderField::Resolver => "resolver_id",
+        DomainOrderField::ResolverId => "resolver_id",
+        DomainOrderField::ResolverAddress => {
+            "(select r.address from resolvers r where r.id = domains.resolver_id)"
+        }
+        DomainOrderField::ResolverContentHash => {
+            "(select r.content_hash from resolvers r where r.id = domains.resolver_id)"
+        }
         DomainOrderField::Ttl => "ttl",
         DomainOrderField::IsMigrated => "is_migrated",
         DomainOrderField::CreatedAt => "created_at",
         DomainOrderField::Owner => "owner_id",
+        DomainOrderField::OwnerId => "owner_id",
         DomainOrderField::Registrant => "registrant_id",
+        DomainOrderField::RegistrantId => "registrant_id",
         DomainOrderField::WrappedOwner => "wrapped_owner_id",
+        DomainOrderField::WrappedOwnerId => "wrapped_owner_id",
         DomainOrderField::ExpiryDate => "expiry_date",
+        DomainOrderField::Registration => {
+            "(select r.id from registrations r where r.domain_id = domains.id)"
+        }
+        DomainOrderField::RegistrationId => {
+            "(select r.id from registrations r where r.domain_id = domains.id)"
+        }
+        DomainOrderField::RegistrationRegistrationDate => {
+            "(select r.registration_date from registrations r where r.domain_id = domains.id)"
+        }
+        DomainOrderField::RegistrationExpiryDate => {
+            "(select r.expiry_date from registrations r where r.domain_id = domains.id)"
+        }
+        DomainOrderField::RegistrationCost => {
+            "(select r.cost from registrations r where r.domain_id = domains.id)"
+        }
+        DomainOrderField::RegistrationLabelName => {
+            "(select r.label_name from registrations r where r.domain_id = domains.id)"
+        }
+        DomainOrderField::WrappedDomain => {
+            "(select w.id from wrapped_domains w where w.domain_id = domains.id)"
+        }
+        DomainOrderField::WrappedDomainId => {
+            "(select w.id from wrapped_domains w where w.domain_id = domains.id)"
+        }
+        DomainOrderField::WrappedDomainExpiryDate => {
+            "(select w.expiry_date from wrapped_domains w where w.domain_id = domains.id)"
+        }
+        DomainOrderField::WrappedDomainFuses => {
+            "(select w.fuses from wrapped_domains w where w.domain_id = domains.id)"
+        }
+        DomainOrderField::WrappedDomainName => {
+            "(select w.name from wrapped_domains w where w.domain_id = domains.id)"
+        }
+        DomainOrderField::Events => domain_event_count_sql(),
     }
 }
 
@@ -49,11 +122,38 @@ pub(crate) fn registration_order_column(order_by: RegistrationOrderField) -> &'s
     match order_by {
         RegistrationOrderField::Id => "id",
         RegistrationOrderField::Domain => "domain_id",
+        RegistrationOrderField::DomainId => "domain_id",
+        RegistrationOrderField::DomainName => {
+            "(select d.name from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainLabelName => {
+            "(select d.label_name from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainLabelhash => {
+            "(select d.labelhash from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainSubdomainCount => {
+            "(select d.subdomain_count from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainTtl => {
+            "(select d.ttl from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainIsMigrated => {
+            "(select d.is_migrated from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainCreatedAt => {
+            "(select d.created_at from domains d where d.id = registrations.domain_id)"
+        }
+        RegistrationOrderField::DomainExpiryDate => {
+            "(select d.expiry_date from domains d where d.id = registrations.domain_id)"
+        }
         RegistrationOrderField::RegistrationDate => "registration_date",
         RegistrationOrderField::ExpiryDate => "expiry_date",
         RegistrationOrderField::Cost => "cost",
         RegistrationOrderField::Registrant => "registrant_id",
+        RegistrationOrderField::RegistrantId => "registrant_id",
         RegistrationOrderField::LabelName => "label_name",
+        RegistrationOrderField::Events => registration_event_count_sql(),
     }
 }
 
@@ -61,9 +161,35 @@ pub(crate) fn wrapped_domain_order_column(order_by: WrappedDomainOrderField) -> 
     match order_by {
         WrappedDomainOrderField::Id => "id",
         WrappedDomainOrderField::Domain => "domain_id",
+        WrappedDomainOrderField::DomainId => "domain_id",
+        WrappedDomainOrderField::DomainName => {
+            "(select d.name from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainLabelName => {
+            "(select d.label_name from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainLabelhash => {
+            "(select d.labelhash from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainSubdomainCount => {
+            "(select d.subdomain_count from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainTtl => {
+            "(select d.ttl from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainIsMigrated => {
+            "(select d.is_migrated from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainCreatedAt => {
+            "(select d.created_at from domains d where d.id = wrapped_domains.domain_id)"
+        }
+        WrappedDomainOrderField::DomainExpiryDate => {
+            "(select d.expiry_date from domains d where d.id = wrapped_domains.domain_id)"
+        }
         WrappedDomainOrderField::ExpiryDate => "expiry_date",
         WrappedDomainOrderField::Fuses => "fuses",
         WrappedDomainOrderField::Owner => "owner_id",
+        WrappedDomainOrderField::OwnerId => "owner_id",
         WrappedDomainOrderField::Name => "name",
     }
 }
@@ -72,10 +198,51 @@ pub(crate) fn resolver_order_column(order_by: ResolverOrderField) -> &'static st
     match order_by {
         ResolverOrderField::Id => "id",
         ResolverOrderField::Domain => "domain_id",
+        ResolverOrderField::DomainId => "domain_id",
+        ResolverOrderField::DomainName => {
+            "(select d.name from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainLabelName => {
+            "(select d.label_name from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainLabelhash => {
+            "(select d.labelhash from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainSubdomainCount => {
+            "(select d.subdomain_count from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainTtl => {
+            "(select d.ttl from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainIsMigrated => {
+            "(select d.is_migrated from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainCreatedAt => {
+            "(select d.created_at from domains d where d.id = resolvers.domain_id)"
+        }
+        ResolverOrderField::DomainExpiryDate => {
+            "(select d.expiry_date from domains d where d.id = resolvers.domain_id)"
+        }
         ResolverOrderField::Address => "address",
         ResolverOrderField::Addr => "addr_id",
+        ResolverOrderField::AddrId => "addr_id",
         ResolverOrderField::ContentHash => "content_hash",
+        ResolverOrderField::Texts => "cardinality(texts)",
+        ResolverOrderField::CoinTypes => "cardinality(coin_types)",
+        ResolverOrderField::Events => resolver_event_count_sql(),
     }
+}
+
+fn domain_event_count_sql() -> &'static str {
+    "(select count(*) from (select id from transfer_events where domain_id = domains.id union all select id from new_owner_events where domain_id = domains.id union all select id from new_resolver_events where domain_id = domains.id union all select id from new_ttl_events where domain_id = domains.id union all select id from wrapped_transfer_events where domain_id = domains.id union all select id from name_wrapped_events where domain_id = domains.id union all select id from name_unwrapped_events where domain_id = domains.id union all select id from fuses_set_events where domain_id = domains.id union all select id from expiry_extended_events where domain_id = domains.id) e)"
+}
+
+fn registration_event_count_sql() -> &'static str {
+    "(select count(*) from (select id from name_registered_events where registration_id = registrations.id union all select id from name_renewed_events where registration_id = registrations.id union all select id from name_transferred_events where registration_id = registrations.id) e)"
+}
+
+fn resolver_event_count_sql() -> &'static str {
+    "(select count(*) from (select id from addr_changed_events where resolver_id = resolvers.id union all select id from multicoin_addr_changed_events where resolver_id = resolvers.id union all select id from name_changed_events where resolver_id = resolvers.id union all select id from abi_changed_events where resolver_id = resolvers.id union all select id from pubkey_changed_events where resolver_id = resolvers.id union all select id from text_changed_events where resolver_id = resolvers.id union all select id from contenthash_changed_events where resolver_id = resolvers.id union all select id from interface_changed_events where resolver_id = resolvers.id union all select id from authorisation_changed_events where resolver_id = resolvers.id union all select id from version_changed_events where resolver_id = resolvers.id) e)"
 }
 
 pub(crate) fn event_order_column(order_by: EventOrderField) -> &'static str {
